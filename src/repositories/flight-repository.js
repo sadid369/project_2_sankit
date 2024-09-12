0002ce4c785c4cbe3119fdd7fa44eeac4b8c2ef9
Sadid <66120003+sadid369@users.noreply.github.com>
2024-09-13 01:44:04 +0600

    added complex join for fetching all airports

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,6 +1,8 @@
-const { where } = require("sequelize");
-const { Flight } = require("../models");
+
 const CrudRepository = require("./crud-repository");
+const { Sequelize } = require('sequelize');
+const { Flight, Airplane, Airport, City } = require('../models');
+
 
 class FlightRepository extends CrudRepository {
     constructor() {
@@ -10,7 +12,39 @@ class FlightRepository extends CrudRepository {
     async getAllFlights (filter, sort) {
         const response = await this.model.findAll({
             where: filter,
-            order: sort
+            order: sort,
+            include: [
+
+                {
+                    model: Airplane,
+                    required: true,
+                    as: 'airplaneDetail',
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'departureAirport',
+                    on: {
+                        col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
+                    },
+                    include: {
+                        model: City,
+                        required: true
+                    }
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'arrivalAirport',
+                    on: {
+                        col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
+                    },
+                    include: {
+                        model: City,
+                        required: true
+                    }
+                }
+            ]
         });
         return response;
     }
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -61,6 +61,7 @@ async function getAllFlights (query) {
         const flights = await flightRepository.getAllFlights(customFilter, sortFilter);
         return flights;
     } catch (error) {
+        console.log(error);
         throw new AppError(
             "Cannot fetch all data of all the airport",
             StatusCodes.INTERNAL_SERVER_ERROR
