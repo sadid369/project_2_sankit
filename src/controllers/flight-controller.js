889412489f770354016077799109e988c866251f
Sadid <66120003+sadid369@users.noreply.github.com>
2024-09-15 18:59:41 +0600

    added get flight api

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -35,7 +35,18 @@ async function getAllFlights (req, res) {
         return res.status(error.statusCode).json(ErrorResponse);
     }
 }
+async function getFlight (req, res) {
+    try {
+        const flight = await FlightService.getFlight(req.params.id);
+        SuccessResponse.data = flight;
+        return res.status(StatusCodes.OK).json(SuccessResponse);
+    } catch (error) {
+        ErrorResponse.error = error;
+        return res.status(error.statusCode).json(ErrorResponse);
+    }
+}
 module.exports = {
     createFlight,
-    getAllFlights
+    getAllFlights,
+    getFlight
 };
--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -14,6 +14,10 @@ router.get(
     "/",
     FlightController.getAllFlights
 );
+router.get(
+    "/:id",
+    FlightController.getFlight
+);
 
 
 module.exports = router;
